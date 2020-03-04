ac8494c4f20d4a7b8aeaa405a5ef96ee3175e63a
Vasilij Schneidermann <mail@vasilij.de>
2020-03-04 22:53:01 +0100

    Quote variables for Windows build

--- build-socket-config.bat
+++ build-socket-config.bat
@@ -1,5 +1,5 @@
 @echo off
-%CHICKEN_CSC% -C %CFLAGS% -L %LDFLAGS% socket-features.scm
+"%CHICKEN_CSC%" -C "%CFLAGS%" -L "%LDFLAGS%" socket-features.scm
 echo "(import scheme)" > socket-config.scm
 echo "(import (chicken platform))" >> socket-config.scm
 ./socket-features >> socket-config.scm
